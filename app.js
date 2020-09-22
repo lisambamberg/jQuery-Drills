823a0b6039a39348ecc2f160821d6c49297f1dca
lisambamberg <lisambamberg@gmail.com>
2020-09-21 19:40:17 -0500

    Added enable and disable function to button

--- app.js
+++ app.js
@@ -6,5 +6,11 @@ $(document).ready(function () {
     $("form").submit(function () {
         alert($("input[name=inputInfo]").val());
     });
-})
 
+    $("#btnSubmit").prop("disabled", true);
+    $("input[name=inputInfo]").keyup(function () {
+        if ($(this).val() != "") {
+            $("#btnSubmit").prop("disabled", false);
+        }
+    });
+})
